4ef084d5f81503c4e753bef6ce928dd7470a6b48
Park-Yu-jin <lits111@naver.com>
2026-01-20 14:55:37 +0900

    ttt

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,11 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const Text = import.meta.env.VITE_APP_TEST
-  async function loadData(){
-    const result = await fetch('https://sample--myapp-48d0e.us-east4.hosted.app/')
-    const data = await result.json();
-    setCount(data)
-  }
-  useEffect(()=>{
-    loadData();
-  },[])
+
   return (
     <>
      <div>
-      {count.message}히히 됬지롱크크
+      {count}히히 됬지롱크크
       {Text}
      </div>
     </>
